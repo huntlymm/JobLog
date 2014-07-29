de7b6bc25d259967b3a032d5ca916142f5022903
Huntly Mayo <huntly.mm@gmail.com>
2014-07-29 17:41:28 -0400

    error check locations

--- plugin/send_data.js
+++ plugin/send_data.js
@@ -8,13 +8,20 @@ jobData.position = fullPosition.slice(0,fullPosition.indexOf('job')-1);
 
 jobData.url = location.href;
 
-var jobLocation = document.getElementsByClassName("locations")[0].textContent;
+var jobLocations = document.getElementsByClassName("locations");
+var jobLocation;
+
+if (typeof jobLocations[0] !== "undefined") {
+  jobLocation = jobLocations[0].textContent;
+}
 
 var salary = document.getElementsByClassName("salary")[0].textContent;
 
 salary = salary.replace("Salary", "").replace(/\n/g, "");
 
 jobData.notes = "Location: " + jobLocation + "\n" + "Comp: " + salary;
+
+console.log(jobData);
 
 //send job data to popup.js
 chrome.extension.sendRequest(jobData);
